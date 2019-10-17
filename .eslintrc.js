e36eb35ac9088d3848f5b69db8a77439865fa936
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-10-16 23:35:55 -0500

    Adding fixes to eslint and react native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  plugins: ['react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['plugin:react-native/all'],
+  rules: {
+    'react-native/no-color-literals': 0,
+    'react-native/sort-styles': 0,
+  },
 };
